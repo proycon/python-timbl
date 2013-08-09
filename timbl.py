cb6be30ac472f8f4fe3b542d95e815395dfa84d6
Maarten van Gompel <proycon@anaproy.nl>
2013-08-09 14:13:59 +0200

    test

--- timbl.py
+++ timbl.py
@@ -232,6 +232,7 @@ class TimblClassifier(object):
         else:
             self.api = timblapi.TimblAPI(options, "")
         print("Calling Timbl API : " + options,file=stderr)
+        self.api.learn(b(trainfile))
         if sys.version < '3':
             self.api.test(b(traintestfile), b(self.fileprefix + '.out'),b'')
         else:

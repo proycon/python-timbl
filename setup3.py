f87ff39abcdb4ee19b116f20e0b9ea68c27bbf36
Maarten van Gompel <proycon@anaproy.nl>
2015-09-04 13:42:10 +0200

    version

--- setup3.py
+++ setup3.py
@@ -124,7 +124,7 @@ timblModule = Extension("timblapi", ["src/timblapi.cc"],
 
 setup(
     name="python3-timbl",
-    version="2015.09.04",
+    version="2015.09.04-1",
     description="Python 3 language binding for the Tilburg Memory-Based Learner",
     author="Sander Canisius, Maarten van Gompel",
     author_email="S.V.M.Canisius@uvt.nl, proycon@anaproy.nl",

872944d6564ddd612b705efa1fed64a31e9905b6
Maarten van Gompel <proycon@anaproy.nl>
2020-05-19 20:26:39 +0200

    minor formatting

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ from __future__ import print_function,  unicode_literals, division, absolute_imp
 import timbl
 import os
 
-#We are building a very simple context-aware translator Word Sense Disambiguator for the word "bank", based on the occurrence of some keywords in the same sentence: 
+#We are building a very simple context-aware translator Word Sense Disambiguator for the word "bank", based on the occurrence of some keywords in the same sentence:
 
-# The features are binary and represent presence or absence of certain keywords. We choose: 
+# The features are binary and represent presence or absence of certain keywords. We choose:
 # - money
 # - sit
 # - river
-#They have a value of 0 or 1   (but note that Timbl support string features just as well!) 
+#They have a value of 0 or 1   (but note that Timbl support string features just as well!)
 
 #The classes we predict are:
 # - financial
@@ -35,7 +35,7 @@ classifier.save()
 
 #We start anew and load the classifier again (of course we could have just skipped this and the save step and continued immediately)
 classifier = timbl.TimblClassifier("wsd-bank", "-a 0 -k 1" ) #wsd-bank will be the prefix of any files written for timbl
-classifier.load() #even if this is omitted it will still work, the first classify() call will invoke load() 
+classifier.load() #even if this is omitted it will still work, the first classify() call will invoke load()
 
 #Let's classify an instance:
 classlabel, distribution, distance = classifier.classify( (1,0,0) )
@@ -61,7 +61,7 @@ classifier.load()
 classifier.addinstance("testfile", (1,0,0),'financial' ) #addinstance can be used to add instances to external files (use append() for training)
 classifier.addinstance("testfile", (0,1,0),'furniture' )
 classifier.addinstance("testfile", (0,0,1),'geograpic' )
-classifier.addinstance("testfile", (1,1,0),'geograpic' ) #this one will be wrongly classified as financial & furniture 
+classifier.addinstance("testfile", (1,1,0),'geograpic' ) #this one will be wrongly classified as financial & furniture
 
 classifier.test("testfile")
 

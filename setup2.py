b65d2814d093c52ff942532cb8abb3a57b0b4ec9
Maarten van Gompel <proycon@anaproy.nl>
2020-05-19 20:28:16 +0200

    version bump

--- setup2.py
+++ setup2.py
@@ -193,7 +193,7 @@ timblModule = Extension("timblapi", ["src/timblapi.cc"],
 
 setup(
     name="python-timbl",
-    version="2019.12.09",
+    version="2020.05.19",
     description="Python 2 language binding for the Tilburg Memory-Based Learner",
     author="Sander Canisius, Maarten van Gompel",
     author_email="S.V.M.Canisius@uvt.nl, proycon@anaproy.nl",
